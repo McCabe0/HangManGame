0f93791e436153bf2a1e6b6a784fbf948cb695cb
Jack M <jackmccabe1995@hotmail.co.uk>
2020-05-04 16:25:30 +0100

    Adding in a loop to allow the uuser to continue to guess

--- src/main/java/jack/Prompter.java
+++ src/main/java/jack/Prompter.java
@@ -7,25 +7,32 @@ public class Prompter {
     private GameLogic gameLogic;
 
 
-    public Prompter(GameLogic gameLogic){
+    public Prompter(GameLogic gameLogic) {
         this.gameLogic = gameLogic;
     }
 
-    public boolean promptForGuess(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a letter:  ");
-        String guessInput = scanner.nextLine();
-        char guess = guessInput.charAt(0);
+    public boolean promptForGuess() {
         Boolean isHit = false;
-        try {
-            isHit = gameLogic.applyGuess(guess);
-        } catch (IllegalArgumentException iae){
-            System.out.println(iae.getMessage());
-        }
+        Scanner scanner = new Scanner(System.in);
+        Boolean isAcceptable = false;
+
+        do {
+            System.out.println("Enter a letter:  ");
+            String guessInput = scanner.nextLine();
+            char guess = guessInput.charAt(0);
+
+            try {
+                isHit = gameLogic.applyGuess(guess);
+                isAcceptable = true;
+            } catch (IllegalArgumentException iae) {
+                System.out.printf("%s, Please try again. %n",
+                        iae.getMessage());
+            }
+        } while (!isAcceptable);
         return isHit;
     }
 
-    public void displayProgress(){
+    public void displayProgress() {
         System.out.printf("You have %d tries left to solve %s %n",
                 gameLogic.getRemainingTries(),
                 gameLogic.getCurrentProgress());
